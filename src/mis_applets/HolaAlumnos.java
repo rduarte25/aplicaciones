b575efaec6b34ea752e3420fc57ac9be71dc75ee
Rafael Duarte <elyouus94@gmail.com>
2021-07-15 23:43:52 -0400

    first commit

---
+++ src/mis_applets/HolaAlumnos.java
@@ -0,0 +1,18 @@
+package mis_applets;
+
+import javax.swing.JApplet;
+import javax.swing.JLabel;
+
+public class HolaAlumnos extends JApplet{
+
+	public void init () {
+		
+		JLabel rotulo = new JLabel("Hola Alumnos");
+		
+		add(rotulo);
+		
+		
+		
+	}
+	
+}
